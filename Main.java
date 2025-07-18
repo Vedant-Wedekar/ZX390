fda56dca9a80f2ba4f6d852c8e22742ea792b3ce
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-07-18 22:50:37 +0530

    .....

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Java Example
+public class Main {
+    public static void main(String[] args) {
+        String name = "Vedant";
+        int age = 20;
+        boolean isCoder = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Coder? " + isCoder);
+    }
+}
